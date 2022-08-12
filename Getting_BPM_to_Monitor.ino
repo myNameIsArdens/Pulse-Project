73f5b9407507591a3211e67483122f963667f400
myNameIsArdens <68923557+myNameIsArdens@users.noreply.github.com>
2022-08-12 15:39:44 -0400

    Update Getting_BPM_to_Monitor.ino

--- Getting_BPM_to_Monitor.ino
+++ Getting_BPM_to_Monitor.ino
@@ -1,50 +1,56 @@
-#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
-#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+// Set-up low-level interrupts for most acurate BPM math.
+#define USE_ARDUINO_INTERRUPTS true
+    
+// Includes the PulseSensorPlayground Library.   
+#include <PulseSensorPlayground.h>     
 
-//  Variables
-const int PulseWire = 0;       // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
-const int LED13 = 13;          // The on-board Arduino LED, close to PIN 13.
-int Threshold = 550;           // Determine which Signal to "count as a beat" and which to ignore.
-                               // Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
-                               // Otherwise leave the default "550" value. 
+// PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int PulseWire = 0;
+
+// The on-board Arduino LED, close to PIN 13.      
+const int LED13 = 13; 
+
+/*Determine which Signal to "count as a beat" and which to ignore.         
+ *Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+ *Otherwise leave the default "550" value.
+*/
+int Threshold = 550;
+
+// To keep track when there isn't any pulse                              
 int Timer = 0;
-                               
-PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+// Creates an instance of the PulseSensorPlayground object called "pulseSensor"                               
+PulseSensorPlayground pulseSensor;
 
 void setup() {   
-
-  Serial.begin(9600);          // For Serial Monitor
-
-  // Configure the PulseSensor object, by assigning our variables to it. 
+  Serial.begin(9600);
   pulseSensor.analogInput(PulseWire);   
-  pulseSensor.blinkOnPulse(LED13);       //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.blinkOnPulse(LED13);     
   pulseSensor.setThreshold(Threshold);   
 
   // Double-check the "pulseSensor" object was created and "began" seeing a signal. 
-   if (pulseSensor.begin()) {
+  if (pulseSensor.begin()) {
     Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up,  or on Arduino reset.  
   }
 }
 
 void loop() {
- int myBPM = pulseSensor.getBeatsPerMinute();  // Calls function on our pulseSensor object that returns BPM as an "int".
-                                               // "myBPM" hold this BPM value now. 
-
-  Timer = Timer + 1;
-  delay(1000);
-  //Serial.println(Timer);
-  
+  int myBPM = pulseSensor.getBeatsPerMinute(); // Calls function on our pulseSensor object that returns BPM as an "int".
+                                               // "myBPM" hold this BPM value now.                                              
+  Timer = Timer + 1;                           // Add 1 to "Timer" every second
+  delay(1000);  
    
- if (pulseSensor.sawStartOfBeat()) {            // Constantly test to see if "a beat happened". 
-  Serial.println("♥  A HeartBeat Happened ! "); // If test is "true", print a message "a heartbeat happened".
-  Serial.print("BPM: ");                        // Print phrase "BPM: " 
-  Serial.println(myBPM);                        // Print the value inside of myBPM. 
-  Timer = 0;
+  if (pulseSensor.sawStartOfBeat()) {              // Constantly test to see if "a beat happened". 
+    Serial.println("♥  A HeartBeat Happened ! ");  // If test is "true", print a message "a heartbeat happened".
+    Serial.print("BPM: ");                         // Print phrase "BPM: " 
+    Serial.println(myBPM);                         // Print the value inside of myBPM. 
+    Timer = 0;                                     // If there's a beat reset the timer to zero
  }
 
- if (Timer > 1){
-  Serial.println("No pulse");
+  // If "Timer" has not been reset, there's no pulse
+  if (Timer > 1){
+    Serial.println("No pulse");
  }
  
- delay(20);                    // considered best practice in a simple sketch.
+ delay(20);                    
 }
